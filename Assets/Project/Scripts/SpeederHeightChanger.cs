fbb663be6e36cb7bf4079b8512bd21767fabbab6
Fennec3 <zxmmike@frontier.com>
2020-07-20 20:19:16 -0700

    race track height changer partialy implemented

---
+++ Assets/Project/Scripts/SpeederHeightChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeederHeightChanger : MonoBehaviour
+{
+    [SerializeField] private GameObject lowCollider;
+    [SerializeField] private GameObject highCollider;
+
+    [SerializeField] private SpriteRenderer renderer;
+
+    public void SetHeightHigh()
+    {
+        lowCollider.SetActive(false);
+        highCollider.SetActive(true);
+
+        renderer.sortingOrder = 20;
+    }
+
+    public void SetHeightLow()
+    {
+        highCollider.SetActive(false);
+        lowCollider.SetActive(true);
+
+        renderer.sortingOrder = 10;
+    }
+}

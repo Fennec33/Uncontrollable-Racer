df1580a25180eb381879859cd394e417262fdad6
Fennec3 <zxmmike@frontier.com>
2020-07-22 20:10:27 -0700

    implemented initial version of enemy racer IA. needs refactoring and perfecting still.

---
+++ Assets/Project/Scripts/SpeederAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeederAI : MonoBehaviour
+{
+    [SerializeField] SpeederMovement speeder;
+    [SerializeField] Transform startingWaypoint;
+    [SerializeField] float turningSpeed = 150f;
+    
+    private Vector3 _currentWaypoint;
+    private Vector3 _vectorToTarget;
+
+    private void Start()
+    {
+        _currentWaypoint = startingWaypoint.position;
+    }
+
+    private void Update()
+    {
+        TurnTowardsCurrentWaypoint();
+        Accelerate();
+    }
+
+    private void TurnTowardsCurrentWaypoint()
+    {
+        _vectorToTarget = _currentWaypoint - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(forward: Vector3.forward, upwards: _vectorToTarget);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, turningSpeed * Time.deltaTime);
+    }
+
+    private void Accelerate()
+    {
+        speeder.IsAccelerating = true;
+    }
+
+    public void SetNewWaypoint(Transform newPoint)
+    {
+        _currentWaypoint = newPoint.position;
+    }
+}
---
+++ Assets/Project/Scripts/Waypoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] private Transform nextWaypoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Speeder")
+        {
+            SpeederAI _ai = collision.transform.parent.GetComponent<SpeederAI>();
+
+            if (_ai != null)
+            {
+                _ai.SetNewWaypoint(nextWaypoint);
+            }
+        }
+    }
+}
